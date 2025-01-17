aebbfa7decfaee14ab0badb47c76080efee342ac
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-01-17 07:46:34 +0600

    Update approve.py

--- plugins/Extra/auto_approve/approve.py
+++ plugins/Extra/auto_approve/approve.py
@@ -18,7 +18,7 @@ join_db = JoinReqs
 
 @Client.on_chat_join_request((filters.group | filters.channel))
 async def auto_approve(client, message: ChatJoinRequest):
-    if message.chat.id == AUTH_CHANNEL and join_db().isActive():
+    if message.chat.id == AUTH_CHANNELS and join_db().isActive():
         if REQUEST_TO_JOIN_MODE == False:
             return 
         ap_user_id = message.from_user.id
@@ -362,7 +362,7 @@ async def auto_approve(client, message: ChatJoinRequest):
     if AUTO_APPROVE_MODE == True:
         if not await db.is_user_exist(message.from_user.id):
             await db.add_user(message.from_user.id, message.from_user.first_name)
-        if message.chat.id == AUTH_CHANNEL:
+        if message.chat.id == AUTH_CHANNELS:
             return 
         chat = message.chat 
         user = message.from_user  
