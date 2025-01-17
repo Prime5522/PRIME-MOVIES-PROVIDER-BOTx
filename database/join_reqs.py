02fca2ab7fab3cf9acb8d33af2c22676c89c70c1
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-01-17 12:13:04 +0600

    Update join_reqs.py

--- database/join_reqs.py
+++ database/join_reqs.py
@@ -1,50 +1,81 @@
 import motor.motor_asyncio
-from info import AUTH_CHANNELS, DATABASE_URI
+
+from info import AUTH_CHANNEL, DATABASE_URI
+
+
 
 class JoinReqs:
 
+
+
     def __init__(self):
+
         if DATABASE_URI:
+
             self.client = motor.motor_asyncio.AsyncIOMotorClient(DATABASE_URI)
+
             self.db = self.client["JoinReqs"]
-            self.collections = {str(channel): self.db[str(channel)] for channel in AUTH_CHANNELS}
+
+            self.col = self.db[str(AUTH_CHANNEL)]
+
         else:
+
             self.client = None
+
             self.db = None
-            self.collections = None
+
+            self.col = None
+
+
 
     def isActive(self):
-        return self.client is not None
 
-    async def add_user(self, channel_id, user_id, first_name, username, date):
+        if self.client is not None:
+
+            return True
+
+        else:
+
+            return False
+
+
+
+    async def add_user(self, user_id, first_name, username, date):
+
         try:
-            if str(channel_id) in self.collections:
-                await self.collections[str(channel_id)].insert_one({
-                    "_id": int(user_id),
-                    "user_id": int(user_id),
-                    "first_name": first_name,
-                    "username": username,
-                    "date": date
-                })
-        except Exception as e:
-            print(f"Error adding user to channel {channel_id}: {e}")
 
-    async def get_user(self, channel_id, user_id):
-        if str(channel_id) in self.collections:
-            return await self.collections[str(channel_id)].find_one({"user_id": int(user_id)})
+            await self.col.insert_one({"_id": int(user_id),"user_id": int(user_id), "first_name": first_name, "username": username, "date": date})
 
-    async def get_all_users(self, channel_id):
-        if str(channel_id) in self.collections:
-            return await self.collections[str(channel_id)].find().to_list(None)
+        except:
 
-    async def delete_user(self, channel_id, user_id):
-        if str(channel_id) in self.collections:
-            await self.collections[str(channel_id)].delete_one({"user_id": int(user_id)})
+            pass
 
-    async def delete_all_users(self, channel_id):
-        if str(channel_id) in self.collections:
-            await self.collections[str(channel_id)].delete_many({})
 
-    async def get_all_users_count(self, channel_id):
-        if str(channel_id) in self.collections:
-            return await self.collections[str(channel_id)].count_documents({})
+
+    async def get_user(self, user_id):
+
+        return await self.col.find_one({"user_id": int(user_id)})
+
+
+
+    async def get_all_users(self):
+
+        return await self.col.find().to_list(None)
+
+
+
+    async def delete_user(self, user_id):
+
+        await self.col.delete_one({"user_id": int(user_id)})
+
+
+
+    async def delete_all_users(self):
+
+        await self.col.delete_many({})
+
+
+
+    async def get_all_users_count(self):
+
+        return await self.col.count_documents({})
