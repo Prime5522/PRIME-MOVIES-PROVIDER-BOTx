00047c7155f349964734950cc65ab626e4d64686
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-01-29 22:47:17 +0600

    Update telegraph.py

--- plugins/Extra/telegraph.py
+++ plugins/Extra/telegraph.py
@@ -4,43 +4,48 @@
 
 
 import os
+import requests
 import asyncio
 from pyrogram import Client, filters
 from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
-from telegraph import upload_file
-from utils import get_file_id
 
+def upload_image_requests(image_path):
+    upload_url = "https://envs.sh"
+
+    try:
+        with open(image_path, 'rb') as file:
+            files = {'file': file} 
+            response = requests.post(upload_url, files=files)
+
+            if response.status_code == 200:
+                return response.text.strip() 
+            else:
+                return print(f"Upload failed with status code {response.status_code}")
+
+    except Exception as e:
+        print(f"Error during upload: {e}")
+        return None
 
 @Client.on_message(filters.command("telegraph") & filters.private)
 async def telegraph_upload(bot, update):
-    t_msg = await bot.ask(chat_id = update.from_user.id, text = "Now Send Me Your Photo Or Video Under 5MB To Get Telegraph Link.")
-  #  if not replied:
-  #      await update.reply_text("𝚁𝙴𝙿𝙻𝚈 𝚃𝙾 𝙰 𝙿𝙷𝙾𝚃𝙾 𝙾𝚁 𝚅𝙸𝙳𝙴𝙾 𝚄𝙽𝙳𝙴𝚁 𝟻𝙼𝙱.")
- #       return
-    file_info = get_file_id(t_msg)
-    if not file_info:
-        await update.reply_text("Not supported!")
+    t_msg = await bot.ask(chat_id = update.from_user.id, text = "Now Send Me Your Photo Or Video Under 5MB To Get Media Link.")
+    if not t_msg.media:
+        return await update.reply_text("**Only Media Supported.**")
+    path = await t_msg.download()
+    uploading_message = await update.reply_text("<b>ᴜᴘʟᴏᴀᴅɪɴɢ...</b>")
+    try:
+        image_url = upload_image_requests(path)
+        if not image_url:
+            return await uploading_message.edit_text("**Failed to upload file.**")
+    except Exception as error:
+        await uploading_message.edit_text(f"**Upload failed: {error}**")
         return
-    text = await update.reply_text(text="<code>Downloading to My Server ...</code>", disable_web_page_preview=True)   
-    media = await t_msg.download()   
-    await text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)                                            
-    try:
-        response = upload_file(media)
-    except Exception as error:
-        print(error)
-        await text.edit_text(text=f"Error :- {error}", disable_web_page_preview=True)       
-        return    
-    try:
-        os.remove(media)
-    except Exception as error:
-        print(error)
-        return    
-    await text.edit_text(
-        text=f"<b>Link :-</b>\n\n<code>https://graph.org{response[0]}</code>",
+    await uploading_message.edit_text(
+        text=f"<b>Link :-</b>\n\n<code>{image_url}</code>",
         disable_web_page_preview=True,
         reply_markup=InlineKeyboardMarkup( [[
-            InlineKeyboardButton(text="Open Link", url=f"https://graph.org{response[0]}"),
-            InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://graph.org{response[0]}")
+            InlineKeyboardButton(text="Open Link", url=image_url),
+            InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url={image_url}")
             ],[
             InlineKeyboardButton(text="✗ Close ✗", callback_data="close")
             ]])
