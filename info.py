97fd4926af5f67cb4fb9898056e8fe937c166cb8
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-02-01 16:58:12 +0600

    Update info.py

--- info.py
+++ info.py
@@ -40,8 +40,30 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
 REQUEST_TO_JOIN_MODE = bool(environ.get('REQUEST_TO_JOIN_MODE', True)) # Set True Or False
 TRY_AGAIN_BTN = bool(environ.get('TRY_AGAIN_BTN', True)) # Set True Or False (This try again button is only for request to join fsub not for normal fsub)
-auth_channel = environ.get('AUTH_CHANNEL', '-1002393090851') # give your force subscribe channel id here else leave it blank
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+
+
+auth_channels = environ.get('AUTH_CHANNEL', '-1002245813234 -1002043502363')
+
+# যদি AUTH_CHANNEL ফাঁকা না থাকে, তাহলে একাধিক চ্যানেল সাপোর্ট করা হবে
+if auth_channels:
+    AUTH_CHANNEL = []
+    for ch in auth_channels.split():
+        # যাচাই করুন যে এটি একটি সঠিক চ্যানেল আইডি কিনা
+        if id_pattern.search(ch):
+            try:
+                AUTH_CHANNEL.append(int(ch))  # সঠিক আইডি হলে সেটি int তে রূপান্তরিত হবে
+            except ValueError:
+                AUTH_CHANNEL.append(ch)  # যদি আইডি int এ রূপান্তর না হয়, তবে এটি স্ট্রিং হিসেবে রাখা হবে
+        else:
+            AUTH_CHANNEL.append(ch)  # যদি এটি সঠিক প্যাটার্ন না হয়, তবে সেটি স্ট্রিং হিসেবে রাখা হবে
+else:
+    AUTH_CHANNEL = None  # যদি কোনো চ্যানেল আইডি না থাকে, তবে None থাকবে
+
+#auth_channel = environ.get('AUTH_CHANNEL', '-1002393090851') # give your force subscribe channel id here else leave it blank
+#AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
+
+
+
 reqst_channel = environ.get('REQST_CHANNEL_ID', '-1004599074149')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 support_chat_id = environ.get('SUPPORT_CHAT_ID', '-1002395758041')
